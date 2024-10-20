fa62d6f1f0e730ad1f893be66fce57bd3fe051e8
Parag Gupta <77960729+Parag949@users.noreply.github.com>
2024-10-20 21:57:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const video = document.getElementById('bg-video');
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
 
-document.addEventListener('keydown', (event) => {
-    if (event.code === 'Space') {
-        if (video.paused) {
-            video.play();
-        } else {
-            video.pause();
-        }
+    // Hide all tab content by default
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
+
+    // Remove the active class from all buttons
+    tablinks = document.getElementsByClassName("tab-btn");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// By default, open the software tab when the page loads
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".tab-btn").click();
 });
