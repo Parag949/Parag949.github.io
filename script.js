07ca8d1f832ae4a93c3d7b0d292e230da7166829
Parag Gupta <77960729+Parag949@users.noreply.github.com>
2024-10-20 22:06:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
 
-    // Hide all tab content by default
+    // Hide all tab content
     tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Remove the active class from all buttons
+    // Remove active class from all buttons
     tablinks = document.getElementsByClassName("tab-btn");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
+    // Show current tab content and add "active" class to clicked button
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
-// By default, open the software tab when the page loads
+// Open Software tab by default
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector(".tab-btn").click();
 });
