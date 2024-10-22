975343c4d5d1ba1261f21b1708a8c45ef5607588
Parag Gupta <77960729+Parag949@users.noreply.github.com>
2024-10-22 10:23:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
 
     // Hide all tab content
-    tabcontent = document.getElementsByClassName("tab-container");
+    tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
@@ -25,3 +25,8 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+// Show default tab
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".tab-btn").click();
+});
